a436a793c1fff5542be18a0d112aebe85147454a
cdmsacc-spec <cdmsacc@gmail.com>
2025-10-18 10:53:57 +0700

    Update AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -115,7 +115,7 @@ class AdminPanelProvider extends PanelProvider
                 FilamentEditProfilePlugin::make()
                     ->shouldShowAvatarForm(
                         value: true,
-                        directory: 'public/user/profile',
+                        directory: 'user/profile',
                         rules: 'mimes:jpeg,png'
                     )
                     ->canAccess(fn() => auth()->user()?->can('page_EditProfilePage'))
