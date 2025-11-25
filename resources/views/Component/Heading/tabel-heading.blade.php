65520b070e088c3746d6d974300d5f9dfc29a839
yandev2 <ryanhappyjalay2461@gmail.com>
2025-11-25 08:33:56 +0700

    release new 4

--- resources/views/Component/Heading/tabel-heading.blade.php
+++ resources/views/Component/Heading/tabel-heading.blade.php
@@ -1,6 +1,6 @@
 <div
     class="custom-heading text-gray-900 dark:text-gray-100"
-    style="font-size:16px; font-weight:600;"
+    style="font-size:16px; font-weight:600; "
 >
     {{ $title }}
 </div>
